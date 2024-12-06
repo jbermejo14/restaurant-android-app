a26610e86eff3b9c7b7b2f629c92e66183ab88d2
jbermejo14 <jpbermejobramley@gmail.com>
2024-12-06 16:35:18 +0000

    Fixed bugs

--- app/src/main/java/com/example/restaurantapp/view/RestaurantListView.java
+++ app/src/main/java/com/example/restaurantapp/view/RestaurantListView.java
@@ -1,16 +1,21 @@
 package com.example.restaurantapp.view;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.example.restaurantapp.MapActivity;
 import com.example.restaurantapp.R;
 import com.example.restaurantapp.adapter.RestaurantAdapter;
 import com.example.restaurantapp.contract.RestaurantListContract;
 import com.example.restaurantapp.domain.Restaurant;
 import com.example.restaurantapp.presenter.RestaurantListPresenter;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +29,8 @@ public class RestaurantListView extends AppCompatActivity implements RestaurantL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_activity);
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         presenter = new RestaurantListPresenter(this);
         presenter.loadRestaurants();
@@ -37,6 +44,25 @@ public class RestaurantListView extends AppCompatActivity implements RestaurantL
 
         restaurantAdapter = new RestaurantAdapter(restaurantList);
         restaurantView.setAdapter(restaurantAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NotNull android.view.MenuItem item) {
+        if (item.getItemId() == R.id.action_map) {
+            Intent intent = new Intent(this, MapActivity.class);
+            startActivity(intent);
+        } else if (item.getItemId()  == R.id.action_register_customer) {
+            Intent intent = new Intent(this, RestaurantListView.class);
+            startActivity(intent);
+        }
+
+        return true;
     }
 
     @Override
